c376c4527b3c03e65faf8da6bbef89f4dc8c8a26
Maxence Ouvrard <22203690@me.uvsq.fr>
2024-10-25 09:16:44 +0200

    [FEATURE] - Réalisation du TP semaphore

--- TP2_Affichage/TP2_Affichage/Affichage.java
+++ TP2_Affichage/TP2_Affichage/Affichage.java
@@ -7,21 +7,21 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
-class Exclusion{};
-public class Affichage extends Thread{
+public class Affichage extends Thread
+{
 	String texte; 
-        
-     static Exclusion exclusionMutuelle = new Exclusion();
+	static semaphoreBinaire sem = new semaphoreBinaire(1);
 
 	public Affichage (String txt){texte=txt;}
 	
-	public void run(){
-
-	    synchronized (exclusionMutuelle) { //section critique
-	    for (int i=0; i<texte.length(); i++){
-		    System.out.print(texte.charAt(i));
-		    try {sleep(100);} catch(InterruptedException e){};
+	public void run()
+	{
+	    sem.syncWait();
+		for (int i=0; i<texte.length(); i++)
+		{
+			System.out.print(texte.charAt(i));
+			try {sleep(100);} catch(InterruptedException e){};
 		}
-	    }
+	    sem.syncSignal();
 	}
 }
--- TP2_Affichage/TP2_Affichage/semaphore.java
+++ TP2_Affichage/TP2_Affichage/semaphore.java
@@ -1,21 +1,31 @@
-public abstract class semaphore {
+public abstract class semaphore
+{
 
     protected int valeur=0;
 
-    protected semaphore (int valeurInitiale){
-	valeur = valeurInitiale>0 ? valeurInitiale:0;
+    protected semaphore (int valeurInitiale)
+    {
+	    valeur = valeurInitiale>0 ? valeurInitiale:0;
     }
 
-    public synchronized void syncWait(){
-	try {
-	    while(valeur<=0){
-		wait();
+    public synchronized void syncWait()
+    {
+        try
+        {
+            while(valeur<=0)
+            {
+                wait();
+            }
+            valeur--;
         }
-	    valeur--;
-	} catch(InterruptedException e){}
+        catch(InterruptedException e)
+        {
+            
+        }
     }
 
-    public synchronized void syncSignal(){
-	if(++valeur > 0) notifyAll();
+    public synchronized void syncSignal()
+    {
+	    if(++valeur > 0) notifyAll();
     }
 }
