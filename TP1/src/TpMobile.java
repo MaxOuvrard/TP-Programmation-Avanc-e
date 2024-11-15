19a2b6f96a0f375cff4137ab10e4be843d1b8d20
Maxence Ouvrard <22203690@me.uvsq.fr>
2024-11-15 09:00:32 +0100

    [FEATURE] - TP1 Ajout des mobiles multiples

--- TP1/src/TpMobile.java
+++ TP1/src/TpMobile.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 
 public class TpMobile
 {
-    public static void main(String[] telsArgs) 
-    {
-	new UneFenetre();
- 	
+    public static void main(String[] telsArgs){
+	    new UneFenetre();
     }
 }
--- TP1/src/UneFenetre.java
+++ TP1/src/UneFenetre.java
@@ -5,25 +5,50 @@ import javax.swing.*;
 
 class UneFenetre extends JFrame implements ActionListener {
     UnMobile sonMobile;
-    Thread laTache;
-    JButton boutonStartStop;
+    UnMobile sonMobile2;
+    UnMobile sonMobile3;
+    UnMobile sonMobile4;
+    Thread laTache1;
+    Thread laTache2;
+    Thread laTache3;
+    Thread laTache4;
+    // JButton boutonStartStop;
     private final int LARG = 1200, HAUT = 700;
+    private final int MOBILE_LARG = 1200, MOBILE_HAUT = 50;
     private boolean estEnCours = true;
 
     public UneFenetre() {
         super("le Mobile");
         Container leContaineur = getContentPane();
-        sonMobile = new UnMobile(LARG, HAUT);
-        leContaineur.add(sonMobile, BorderLayout.CENTER);
+        leContaineur.setLayout(new GridLayout(4,1));
+
+        sonMobile = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
+        leContaineur.add(sonMobile);
+
+        sonMobile2 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
+        leContaineur.add(sonMobile2);
+
+        sonMobile3 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
+        leContaineur.add(sonMobile3);
+
+        sonMobile4 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
+        leContaineur.add(sonMobile4);
 
         // Création du bouton
-        boutonStartStop = new JButton("Start/Stop");
-        boutonStartStop.addActionListener(this);
-        leContaineur.add(boutonStartStop, BorderLayout.SOUTH);
+        // boutonStartStop = new JButton("Start/Stop");
+        // boutonStartStop.addActionListener(this);
+        // leContaineur.add(boutonStartStop, BorderLayout.SOUTH);
 
         // Démarrer le thread
-        laTache = new Thread(sonMobile);
-        laTache.start();
+        laTache1 = new Thread(sonMobile);
+        laTache2 = new Thread(sonMobile2);
+        laTache3 = new Thread(sonMobile3);
+        laTache4 = new Thread(sonMobile4);
+
+        laTache1.start();
+        laTache2.start();
+        laTache3.start();
+        laTache4.start();
 
         setSize(LARG, HAUT);
         setVisible(true);
@@ -33,9 +58,9 @@ class UneFenetre extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (estEnCours) {
-            laTache.suspend();  // Méthode dépréciée
+            laTache1.suspend();  // Méthode dépréciée
         } else {
-            laTache.resume();  // Méthode dépréciée
+            laTache1.resume();  // Méthode dépréciée
         }
         estEnCours = !estEnCours;
     }
