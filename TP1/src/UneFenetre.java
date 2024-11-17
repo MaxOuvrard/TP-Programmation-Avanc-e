1e2336a1c71c0d16892b9aafae033d3f7eb43ac5
MaxOuvrard <118462372+MaxOuvrard@users.noreply.github.com>
2024-11-17 22:46:43 +0100

    [FEATURE] - Rajout des sémaphores TP1

--- TP1/src/UneFenetre.java
+++ TP1/src/UneFenetre.java
@@ -1,9 +1,8 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.util.concurrent.Semaphore;
 
-class UneFenetre extends JFrame implements ActionListener {
+class UneFenetre extends JFrame {
     UnMobile sonMobile;
     UnMobile sonMobile2;
     UnMobile sonMobile3;
@@ -12,34 +11,27 @@ class UneFenetre extends JFrame implements ActionListener {
     Thread laTache2;
     Thread laTache3;
     Thread laTache4;
-    // JButton boutonStartStop;
+
     private final int LARG = 1200, HAUT = 700;
     private final int MOBILE_LARG = 1200, MOBILE_HAUT = 50;
-    private boolean estEnCours = true;
+
+    public static final Semaphore zone2Semaphore = new Semaphore(1);
 
     public UneFenetre() {
         super("le Mobile");
         Container leContaineur = getContentPane();
-        leContaineur.setLayout(new GridLayout(4,1));
+        leContaineur.setLayout(new GridLayout(4, 1));
 
         sonMobile = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
-        leContaineur.add(sonMobile);
-
         sonMobile2 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
-        leContaineur.add(sonMobile2);
-
         sonMobile3 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
-        leContaineur.add(sonMobile3);
-
         sonMobile4 = new UnMobile(MOBILE_LARG, MOBILE_HAUT);
+
+        leContaineur.add(sonMobile);
+        leContaineur.add(sonMobile2);
+        leContaineur.add(sonMobile3);
         leContaineur.add(sonMobile4);
 
-        // Création du bouton
-        // boutonStartStop = new JButton("Start/Stop");
-        // boutonStartStop.addActionListener(this);
-        // leContaineur.add(boutonStartStop, BorderLayout.SOUTH);
-
-        // Démarrer le thread
         laTache1 = new Thread(sonMobile);
         laTache2 = new Thread(sonMobile2);
         laTache3 = new Thread(sonMobile3);
@@ -53,15 +45,5 @@ class UneFenetre extends JFrame implements ActionListener {
         setSize(LARG, HAUT);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (estEnCours) {
-            laTache1.suspend();  // Méthode dépréciée
-        } else {
-            laTache1.resume();  // Méthode dépréciée
-        }
-        estEnCours = !estEnCours;
     }
 }
