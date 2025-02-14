6c9d6c8d1f426463c76c07625a1496bd49ba5b30
MaxOuvrard <maxenceouvrard.pro@gmail.com>
2025-02-14 10:16:03 +0100

    [FEATURE] - Ajout graphes Assignment102.java

--- TP4/Partie_2/Assignment102.java
+++ TP4/Partie_2/Assignment102.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 class PiMonteCarlo {
 	AtomicInteger nAtomSuccess;
+	int nProcessors;
 	int nThrows;
 	double value;
 	class MonteCarlo implements Runnable {
@@ -17,13 +18,14 @@ class PiMonteCarlo {
 				nAtomSuccess.incrementAndGet();
 		}
 	}
-	public PiMonteCarlo(int i) {
+	public PiMonteCarlo(int i, int processors) {
 		this.nAtomSuccess = new AtomicInteger(0);
 		this.nThrows = i;
+		this.nProcessors = processors;
 		this.value = 0;
 	}
 	public double getPi() {
-		int nProcessors = Runtime.getRuntime().availableProcessors();
+		// int nProcessors = Runtime.getRuntime().availableProcessors();
 		ExecutorService executor = Executors.newWorkStealingPool(nProcessors);
 		for (int i = 1; i <= nThrows; i++) {
 			Runnable worker = new MonteCarlo();
@@ -38,23 +40,26 @@ class PiMonteCarlo {
 }
 public class Assignment102 {
 	public static void main(String[] args) {
-		PiMonteCarlo PiVal = new PiMonteCarlo(12000000);
+		int nThrows = 12000000;
+		int nProcessors = 12;
+		PiMonteCarlo PiVal = new PiMonteCarlo(nThrows,nProcessors);
 		long startTime = System.currentTimeMillis();
 		double value = PiVal.getPi();
 		long stopTime = System.currentTimeMillis();
 		System.out.println("Approx value:" + value);
 		System.out.println("Difference to exact value of pi: " + (value - Math.PI));
 		System.out.println("Error: " + (value - Math.PI) / Math.PI * 100 + " %");
-		System.out.println("Available processors: " + Runtime.getRuntime().availableProcessors());
+		System.out.println("Available processors: " + nProcessors);
 		System.out.println("Time Duration: " + (stopTime - startTime) + "ms");
 
 
 		try {
 			FileWriter writer = new FileWriter("out-assignements102.txt", true);
 			writer.write(
-					"" + Runtime.getRuntime().availableProcessors() + " " +
+					"" + PiVal.nThrows + " " +
 					(stopTime - startTime) + " " +
-					PiVal.nThrows + "\n");
+					(Math.abs((PiVal.value - Math.PI)) / Math.PI) + " " + 
+					nProcessors + "\n");
 			writer.close();
 		} catch (IOException e) {
 			System.out.println("An error occurred while writing to the file.");
